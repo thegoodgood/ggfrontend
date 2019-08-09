ffb74db5077ea349c37584a70a9b987e10796cec
Jasterix <ljcalixte@gmail.com>
2019-08-08 20:14:59 -0400

    Updates

---
+++ precache-manifest.3322657b93ceb40a5bb990f0612b2ac1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1120ebd20751c41119d84e3b7ec6b724",
+    "url": "https://thegoodgood.github.io/ggfrontend/index.html"
+  },
+  {
+    "revision": "d3b9994e0c0b6b908bda",
+    "url": "https://thegoodgood.github.io/ggfrontend/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "d900a2f8fbc18bb5d8ef",
+    "url": "https://thegoodgood.github.io/ggfrontend/static/css/main.efabf909.chunk.css"
+  },
+  {
+    "revision": "d3b9994e0c0b6b908bda",
+    "url": "https://thegoodgood.github.io/ggfrontend/static/js/2.23d3eab1.chunk.js"
+  },
+  {
+    "revision": "d900a2f8fbc18bb5d8ef",
+    "url": "https://thegoodgood.github.io/ggfrontend/static/js/main.cc6a32bb.chunk.js"
+  },
+  {
+    "revision": "df8834ef05605a42612e",
+    "url": "https://thegoodgood.github.io/ggfrontend/static/js/runtime~main.de5656e4.js"
+  },
+  {
+    "revision": "67c2ec815d801e3bd27cf427bc2e56c3",
+    "url": "https://thegoodgood.github.io/ggfrontend/static/media/springbanner.67c2ec81.jpeg"
+  }
+]);
