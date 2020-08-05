a49de28702185376ba7c6eaa04572e179e3e7fdd
Jasterix <ljcalixte@gmail.com>
2020-08-05 15:08:55 -0400

    Updates

---
+++ precache-manifest.2c5758191eb518b4631b4cf4beda9760.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c15167faf79b5bb153d32d438974a30b",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "ac1064a7d238f32538d4",
+    "url": "/home/static/css/2.ceb79488.chunk.css"
+  },
+  {
+    "revision": "486f9e6382df279a05ea",
+    "url": "/home/static/css/main.e6654203.chunk.css"
+  },
+  {
+    "revision": "ac1064a7d238f32538d4",
+    "url": "/home/static/js/2.ac1c024b.chunk.js"
+  },
+  {
+    "revision": "ecdf0eaf869245bc3daa3187fcc23d88",
+    "url": "/home/static/js/2.ac1c024b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "486f9e6382df279a05ea",
+    "url": "/home/static/js/main.bbd18388.chunk.js"
+  },
+  {
+    "revision": "eb268d7b32c176ffbd06",
+    "url": "/home/static/js/runtime-main.164d213a.js"
+  },
+  {
+    "revision": "67c2ec815d801e3bd27cf427bc2e56c3",
+    "url": "/home/static/media/springbanner.67c2ec81.jpeg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/home/precache-manifest.aad400eacfe93b600288a9cea250bfae.js"
+  "/home/precache-manifest.2c5758191eb518b4631b4cf4beda9760.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/home/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
