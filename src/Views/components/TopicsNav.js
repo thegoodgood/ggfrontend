70e1eb2a3d58a9908be311727a6ac7d2a990cc97
Jasterix <ljcalixte@gmail.com>
2020-08-05 09:33:13 -0400

    update packages and change localhosst to heroku

--- src/Views/components/TopicsNav.js
+++ src/Views/components/TopicsNav.js
@@ -1,52 +1,53 @@
-import React from "react"
-import Tweet from "../components/Tweet"
-import { withRouter } from "react-router-dom"
+import React from "react";
+import Tweet from "../components/Tweet";
+import { withRouter } from "react-router-dom";
 
 import {
   getTopicAction,
-  getTopicTweetsAction
-} from "../../redux/actions/tweetAdapter"
-import { getTopic } from "../../redux/actions/topicActions"
-import { connect } from "react-redux"
+  getTopicTweetsAction,
+} from "../../redux/actions/tweetAdapter";
+import { getTopic } from "../../redux/actions/topicActions";
+import { connect } from "react-redux";
 
 class TopicsNav extends React.Component {
   componentDidMount() {
-    this.props.setTopic("home")
+    this.props.setTopic("home");
   }
 
-  handleClick = event => {
-    this.props.setTopic(event.target.innerText)
-    this.props.getTopicTweets(event.target.innerText)
-  }
+  handleClick = (event) => {
+    this.props.setTopic(event.target.innerText);
+    this.props.getTopicTweets(event.target.innerText);
+  };
 
   goHome = (event) => {
-    const { history } = this.props
-    if(history) history.push('/')
-  }
+    const { history } = this.props;
+    if (history) history.push("/");
+  };
 
   render() {
     return (
-      <div className="topicsNav">
-        <button onClick={this.handleClick}> News</button>
+      <div className='topicsNav'>
+        {/* <button onClick={this.handleClick}> News</button> */}
         <button onClick={this.handleClick}>Social Commentary</button>
         <button onClick={this.handleClick}>On the Daily</button>
-        <button className= "all-tweets" onClick={this.goHome}>All Tweets</button>
+        <button className='all-tweets' onClick={this.goHome}>
+          All Tweets
+        </button>
       </div>
-    )
+    );
   }
 }
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   return {
-    topic: state.topic
-  }
-}
+    topic: state.topic,
+  };
+};
 
 const mapDispatchToProps = {
   setTopic: getTopicAction,
-  getTopicTweets: getTopicTweetsAction
-}
+  getTopicTweets: getTopicTweetsAction,
+};
 
-export default withRouter(connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(TopicsNav))
+export default withRouter(
+  connect(mapStateToProps, mapDispatchToProps)(TopicsNav)
+);
--- src/redux/actions/tweetAdapter.js
+++ src/redux/actions/tweetAdapter.js
@@ -1,148 +1,151 @@
 import {
-  getTweetsStart, getTopicTweetsStart, getTweetsSuccess, getTweetsFailure
-} from './tweetActions'
+  getTweetsStart,
+  getTopicTweetsStart,
+  getTweetsSuccess,
+  getTweetsFailure,
+} from "./tweetActions";
 
 import {
-getTopic,  getTopicStart, getTopicSuccess, getTopicFailure
-} from './topicActions'
+  getTopic,
+  getTopicStart,
+  getTopicSuccess,
+  getTopicFailure,
+} from "./topicActions";
 
-
-import {BASE_URL} from '../../apiConstants'
-const tweets_url = "http://localhost:3000/tweets"
-const get_tweets_url = "http://localhost:3000/get_"
-const url = "http://localhost:3000"
-
+import { BASE_URL } from "../../apiConstants";
+const tweets_url = "https://thegoodgood.herokuapp.com/tweets";
+const get_tweets_url = "https://thegoodgood.herokuapp.com/get_";
+const url = "https://thegoodgood.herokuapp.com/";
 
 //--------------------------------------FETCH ALL TWEETS
-  export const getTweetsAction = () => dispatch => {
-    dispatch(getTweetsStart())
-      return fetch(tweets_url)
-      .then(res => res.json())
-      .then(tweets => {
-        dispatch(getTweetsSuccess(tweets))
-        })
-        .catch(error => {
-          dispatch(getTweetsFailure(error))
-        })
-      }
+export const getTweetsAction = () => (dispatch) => {
+  dispatch(getTweetsStart());
+  return fetch(tweets_url)
+    .then((res) => res.json())
+    .then((tweets) => {
+      dispatch(getTweetsSuccess(tweets));
+    })
+    .catch((error) => {
+      dispatch(getTweetsFailure(error));
+    });
+};
 
 //-----------------------FETCH BY TOPIC TWEETS
-      export const getTopicTweetsAction = (topic) => dispatch => {
-        let new_topic = topic.toLowerCase().split(" ").join("")
-        dispatch(getTopicTweetsStart(new_topic))
-          return fetch(`${get_tweets_url}${new_topic}`)
-          .then(res => res.json())
-          .then(tweets => {
-            dispatch(getTweetsSuccess(tweets))
-            })
-            .catch(error => {
-              dispatch(getTweetsFailure(error))
-            })
-          }
+export const getTopicTweetsAction = (topic) => (dispatch) => {
+  let new_topic = topic.toLowerCase().split(" ").join("");
+  dispatch(getTopicTweetsStart(new_topic));
+  return fetch(`${get_tweets_url}${new_topic}`)
+    .then((res) => res.json())
+    .then((tweets) => {
+      dispatch(getTweetsSuccess(tweets));
+    })
+    .catch((error) => {
+      dispatch(getTweetsFailure(error));
+    });
+};
 
 //-------------FETCH NEWS
-      export const getNewsAction = () => dispatch => {
-        dispatch(getTopicTweetsStart())
-          return fetch(tweets_url`/news`)
-          .then(res => res.json())
-          .then(tweets => {
-            dispatch(getTweetsSuccess(tweets))
-            })
-            .catch(error => {
-              dispatch(getTweetsFailure(error))
-            })
-          }
+export const getNewsAction = () => (dispatch) => {
+  dispatch(getTopicTweetsStart());
+  return fetch(tweets_url`/news`)
+    .then((res) => res.json())
+    .then((tweets) => {
+      dispatch(getTweetsSuccess(tweets));
+    })
+    .catch((error) => {
+      dispatch(getTweetsFailure(error));
+    });
+};
 
 //-------------FETCH EMBEDDED TWEET
 
-  // export const embedTweetAction = () => dispatch => {
-  //   fetch("https://publish.twitter.com/oembed?url=https%3A%2F%2Ftwitter.com%2FInterior%2Fstatus%2F507185938620219395'", {
-  //      mode: 'no-cors',
-  //      "X-Frame-Options": "allow-from"
-  //    })
-  //   .then(res => res.json())
-  //   .then(data => {
-  //     return data
-  //   })
-  //   .catch(error => {
-  //     return error
-  //     })
-  // }
+// export const embedTweetAction = () => dispatch => {
+//   fetch("https://publish.twitter.com/oembed?url=https%3A%2F%2Ftwitter.com%2FInterior%2Fstatus%2F507185938620219395'", {
+//      mode: 'no-cors',
+//      "X-Frame-Options": "allow-from"
+//    })
+//   .then(res => res.json())
+//   .then(data => {
+//     return data
+//   })
+//   .catch(error => {
+//     return error
+//     })
+// }
 
 //-----------------------UPDATE TOPIC IN THE STORE
-export const getTopicAction = (topic) => dispatch => {
-    dispatch(getTopic(topic))
-    }
-
+export const getTopicAction = (topic) => (dispatch) => {
+  dispatch(getTopic(topic));
+};
 
 // -----------------------------------GET THE CURRENT TWEET
-export const getCurrentTweetAction = (id) => dispatch => {
-  dispatch({ type: "GET_CURRENT_TWEET_START" })
+export const getCurrentTweetAction = (id) => (dispatch) => {
+  dispatch({ type: "GET_CURRENT_TWEET_START" });
 
   return fetch(`${tweets_url}/${id}`, {
     headers: {
-      "Authorization": `Bearer ${localStorage.token}`
-    }
+      Authorization: `Bearer ${localStorage.token}`,
+    },
   })
-  .then(res => res.json())
-  .then(tweet => {
-    dispatch({ type: 'GET_CURRENT_TWEET_SUCCESS', tweet: tweet })
-  })
-  .catch(error => {
-    dispatch({ type: 'GET_CURRENT_TWEET_FAILURE', error: error })
-  })
-}
+    .then((res) => res.json())
+    .then((tweet) => {
+      dispatch({ type: "GET_CURRENT_TWEET_SUCCESS", tweet: tweet });
+    })
+    .catch((error) => {
+      dispatch({ type: "GET_CURRENT_TWEET_FAILURE", error: error });
+    });
+};
 
 // ----------------------------------------DELETE THE CURRENT TWEET
 
-export const deleteTweetAction = (tweet) => dispatch => {
-  let id = tweet.id
+export const deleteTweetAction = (tweet) => (dispatch) => {
+  let id = tweet.id;
   fetch(`${tweets_url}/${id}`, {
-    method: 'DELETE',
+    method: "DELETE",
     headers: {
-      "Authorization": `Bearer ${localStorage.token}`
-    }
+      Authorization: `Bearer ${localStorage.token}`,
+    },
   })
-  .then(res => res.json())
-  .then(tweet => {
-    dispatch({ type: 'DELETE_TWEET', tweet: tweet })
-  })
-  .catch(error => {
-    dispatch({ type: 'GET_CURRENT_TWEET_FAILURE', error: error })
-  })
-}
+    .then((res) => res.json())
+    .then((tweet) => {
+      dispatch({ type: "DELETE_TWEET", tweet: tweet });
+    })
+    .catch((error) => {
+      dispatch({ type: "GET_CURRENT_TWEET_FAILURE", error: error });
+    });
+};
 
 // --------------------------------------------------UPVOTE A TWEET
 
-export const upvoteTweetAction = (id) => dispatch => {
-  dispatch({ type: "GET_CURRENT_TWEET_START" })
+export const upvoteTweetAction = (id) => (dispatch) => {
+  dispatch({ type: "GET_CURRENT_TWEET_START" });
   fetch(`${tweets_url}/${id}/upvote`, {
-    method: 'POST',
+    method: "POST",
     headers: {
-      "Authorization": `Bearer ${localStorage.token}`
-    }
+      Authorization: `Bearer ${localStorage.token}`,
+    },
   })
-  .then(res => res.json())
-  .then(tweet => {
-    dispatch({ type: 'GET_CURRENT_TWEET_SUCCESS', tweet: tweet })
-    dispatch({ type: 'ADD_TO_UPVOTED_TWEETS', tweet: tweet })
-  })
-  .catch(error => {
-    dispatch({ type: 'GET_CURRENT_TWEET_FAILURE', error: error })
-  })
-}
+    .then((res) => res.json())
+    .then((tweet) => {
+      dispatch({ type: "GET_CURRENT_TWEET_SUCCESS", tweet: tweet });
+      dispatch({ type: "ADD_TO_UPVOTED_TWEETS", tweet: tweet });
+    })
+    .catch((error) => {
+      dispatch({ type: "GET_CURRENT_TWEET_FAILURE", error: error });
+    });
+};
 
-export const downvoteTweetAction = (id) => dispatch => {
-  dispatch({ type: "GET_CURRENT_TWEET_START" })
+export const downvoteTweetAction = (id) => (dispatch) => {
+  dispatch({ type: "GET_CURRENT_TWEET_START" });
 
   return fetch(`${tweets_url}/${id}/downvote`, {
-    method: 'POST',
+    method: "POST",
     headers: {
-      "Authorization": `Bearer ${localStorage.token}`
-    }
+      Authorization: `Bearer ${localStorage.token}`,
+    },
   })
-  .then(res => res.json())
-  .then(tweet => {
-    dispatch({ type: 'REMOVE_FROM_UPVOTED_TWEETS', tweet: tweet })
-  })
-}
+    .then((res) => res.json())
+    .then((tweet) => {
+      dispatch({ type: "REMOVE_FROM_UPVOTED_TWEETS", tweet: tweet });
+    });
+};
